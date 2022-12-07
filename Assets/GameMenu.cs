fb58e0306dfcd573629466df2830496edff3d6d9
abbrobvik <92145330+abbrobvik@users.noreply.github.com>
2022-12-07 06:20:27 +0100

    .

--- Assets/GameMenu.cs
+++ Assets/GameMenu.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMenu : MonoBehaviour
 {
-    public InGameMenu inGameMenu;
+    //Attach your canvas to this variable in inspector
+    public GameObject canvasObj;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //This will check if your game is paused (we'll set it)
+    private bool gamePaused;
+
 
     void Update()
     {
-
+        //Reading input for ESCAPE key, and by saying gamePaused = !gamePaused, we switch the bool on and off each time the Keycode is registered!
         if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            inGameMenu.gameObject.SetActive(!inGameMenu.gameObject.activeSelf);
-        }
+            gamePaused = !gamePaused;
+
+        //Now we enable and disable the game object!
+        if (gamePaused)
+            canvasObj.SetActive(true);
+        else
+            canvasObj.SetActive(false);
+
     }
 }
