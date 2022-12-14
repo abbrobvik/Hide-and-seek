61d6fedf6ec67d55abfe1f444c7c1cf47e9112c7
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2022-12-14 13:21:45 +0100

    fix spawn

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+    public Transform spawnpoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.position = spawnpoint.position;
     }
 
     // Update is called once per frame
